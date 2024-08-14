66321b9a00ffc8030aa7adc6f57c2e8ffbde3d88
Asma <saiyedas@shernet.sheridancollege.ca>
2024-08-13 21:16:29 -0400

    String & Char added 9:15PM

---
+++ src/CharArray.java
@@ -0,0 +1,14 @@
+public class CharArray {
+    public static void main(String[] args) {
+
+        char[] name = {'P', 'R', 'A' ,'G', 'R', 'A'};
+
+        for (char element: name) {
+            System.out.print(element);
+        }
+
+
+        // Strings are - character array ...
+
+    }
+}
